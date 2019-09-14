434565d9390b0710c6e9530aeb766fdcc03d79e9
VincentPestana <crashoveride001@gmail.com>
2019-09-14 08:58:57 +0200

    Add project files.

---
+++ SocketShot/StreamManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using Newtonsoft.Json;
+
+namespace SocketShot
+{
+    internal class StreamManager
+    {
+        public StreamManager()
+        {
+            bool capture = true;
+
+            Stopwatch timer = new Stopwatch();
+            var averageTime = 0l;
+
+            var hubConnection = new HubConnection("http://localhost:51628/");
+            var hubProxy = hubConnection.CreateHubProxy("StreamHub");
+            hubConnection.Start().Wait();
+
+            // Initialize
+            Bitmap bitmap;
+            Graphics graphics;
+            var groupName = DateTime.Now.Ticks.ToString();
+            
+
+            bitmap = new Bitmap(100, 100);
+
+            graphics = Graphics.FromImage(bitmap as Image);
+            string b64Bitmap = "";
+
+            while (capture)
+            {
+                timer.Restart();
+
+
+                graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+                using (var ms = new MemoryStream())
+                {
+                    bitmap.Save(ms, ImageFormat.Png);
+                    byte[] byteImage = ms.ToArray();
+                    b64Bitmap = Convert.ToBase64String(byteImage);
+                }
+
+                hubProxy.Invoke("sendScreen", "data:image/png;base64, " + b64Bitmap).Wait();
+
+                timer.Stop();
+                averageTime = (averageTime + timer.ElapsedMilliseconds) / 2;
+                Console.WriteLine(averageTime + "ms");
+            }
+        }
+    }
+}
---
+++ SocketStreamer/StreamHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace SocketStreamer
+{
+    public class StreamHub : Hub
+    {
+        public string SendScreen(string jsonBitmap)
+        {
+            Clients.AllExcept(Context.ConnectionId).sendScreen(jsonBitmap);
+            return "";
+        }
+    }
+}
