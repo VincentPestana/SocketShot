ccba57b5981b07de4a3edea83ff5389c16aa1796
VincentPestana <crashoveride001@gmail.com>
2019-09-15 21:03:16 +0200

    Resize image before sending it List of common image sizes added in comments

--- SocketShot/StreamManager.cs
+++ SocketShot/StreamManager.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.IO.Compression;
 using System.Windows.Forms;
 using Microsoft.AspNet.SignalR.Client;
 using Microsoft.AspNet.SignalR.Client.Hubs;
@@ -15,7 +16,16 @@ namespace SocketShot
 		// Initial starting quality setting
 		private long _qualitySetting = 20L;
 
-		private int _desiredSizeKB = 64;
+		private int _desiredSizePerShotKB = 96;
+
+		// Image resize
+		//	1920 × 1080 
+		//	1280 × 720 
+		//	960 × 540 
+		//	720 × 480 
+		//	640 × 360
+		private int _streamImageWidth = 1280;
+		private int _streamImageHeight = 720;
 
 		public StreamManager()
 		{
@@ -36,16 +46,16 @@ namespace SocketShot
             // Initialize
             Bitmap bitmap;
             Graphics graphics;
+            string b64Bitmap = "";
             
 			// Capture 1920x1080 pixels
             bitmap = new Bitmap(1920, 1080, PixelFormat.Format24bppRgb);
-
             graphics = Graphics.FromImage(bitmap as Image);
-            string b64Bitmap = "";
 
 			// Setup encoder
 			ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
 			EncoderParameters encoderParameters = new EncoderParameters(1);
+
 			// Quality options
 			var qualityEncoder = Encoder.Quality;
 			EncoderParameter qualityEncoderParameter = new EncoderParameter(qualityEncoder, _qualitySetting);
@@ -58,9 +68,12 @@ namespace SocketShot
 
 				graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
 
+				// Resize image
+				var resizedBitmap = ResizeBitmap(bitmap, _streamImageWidth, _streamImageHeight);
+
 				using (var ms = new MemoryStream())
 				{
-					bitmap.Save(ms, jpgEncoder, encoderParameters);
+					resizedBitmap.Save(ms, jpgEncoder, encoderParameters);
 					
 					// Bitmap image to array
 					byte[] byteImage = ms.ToArray();
@@ -68,7 +81,7 @@ namespace SocketShot
 					// Convert to Base64
 					b64Bitmap = Convert.ToBase64String(byteImage);
 				}
-
+				
 				// Send image over SignalR
 				bool sendFailed;
 				try
@@ -91,9 +104,9 @@ namespace SocketShot
 				{
 					// Dynamic Quality adjustment
 					var sizeSent = b64Bitmap.Length / 1024;
-					if (sizeSent > (_desiredSizeKB + 5))
+					if (sizeSent > (_desiredSizePerShotKB + 5))
 						_qualitySetting -= 2L;
-					else if (sizeSent < (_desiredSizeKB - 5))
+					else if (sizeSent < (_desiredSizePerShotKB - 5))
 						_qualitySetting += 2L;
 				}
 
@@ -105,6 +118,17 @@ namespace SocketShot
 
 				Console.WriteLine("FPS:Qual:Size - " + (1000 / averageTime) + " : " + _qualitySetting + " : " + b64Bitmap.Length / 1024 + "Kb" + ((sendFailed) ? " - failed" : ""));
 			}
+		}
+
+		private Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
+		{
+			Bitmap result = new Bitmap(width, height);
+			using (Graphics g = Graphics.FromImage(result))
+			{
+				g.DrawImage(bmp, 0, 0, width, height);
+			}
+
+			return result;
 		}
 
 		private ImageCodecInfo GetEncoder(ImageFormat format)
