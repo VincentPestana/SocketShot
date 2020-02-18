5cf87cba0f72d6f6a50a9eb55c10a5459ebc299f
VincentPestana <crashoveride001@gmail.com>
2020-02-18 19:05:06 +0200

    Less error prone when using user entered urls

--- SocketShot/Program.cs
+++ SocketShot/Program.cs
@@ -10,8 +10,12 @@ namespace SocketShot
 			// Setup SignalR
 			Console.WriteLine("Enter SR url? Hit enter for default: http://localhost:51628/");
 			var enteredUrl = Console.ReadLine();
+			// Check entered url
 			if (string.IsNullOrEmpty(enteredUrl))
 				enteredUrl = "http://localhost:51628/";
+
+			if (!enteredUrl.StartsWith("http://"))
+				enteredUrl = "http://" + enteredUrl;
 
 			var hubConnection = new HubConnection(enteredUrl);
 			var hubProxy = hubConnection.CreateHubProxy("StreamHub");
