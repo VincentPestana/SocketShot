df46646dbadedd0d945a523c1e2dacbc53fbb919
VincentPestana <crashoveride001@gmail.com>
2020-02-20 20:55:19 +0200

    Added more fields to the StreamMeta

--- SocketShot/StreamMeta.cs
+++ SocketShot/StreamMeta.cs
@@ -3,6 +3,14 @@
 	class StreamMeta
 	{
 		public string JsonB64Bitmap { get; set; }
-		
+		public string BitmapEncodeQuality { get; set; }
+		public int CaptureHeight { get; set; }
+		public int CaptureWidth { get; set; }
+		public int StreamHeight { get; set; }
+		public int StreamWidth { get; set; }
+		public float CaptureTimeMilliseconds { get; set; }
+		public int StreamSizeKB { get; set; }
+		public int FramesPerSecond { get; set; }
+		public bool PreviousFrameFailedToSend { get; set; }
 	}
 }
