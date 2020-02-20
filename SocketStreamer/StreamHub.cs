2ff284ad4c7a4d7dfff7965ddba2fa25afc942a6
VincentPestana <crashoveride001@gmail.com>
2020-02-20 20:37:45 +0200

    Added new hub method for new detailed stream object

--- SocketStreamer/StreamHub.cs
+++ SocketStreamer/StreamHub.cs
@@ -8,5 +8,10 @@ namespace SocketStreamer
         {
             Clients.AllExcept(Context.ConnectionId).sendB64Screen(jsonBitmap);
         }
+
+        public void SendScreenDetailed(string jsonImageObject)
+        {
+            Clients.AllExcept(Context.ConnectionId).sendScreenDetailed(jsonImageObject);
+        }
     }
 }
